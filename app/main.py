119ba4949ccb61c4884464227e1b8f7687d706bd
Ruslan Priadko <r.priadko@ukr.net>
2024-12-28 16:33:45 +0200

    format corrected

--- app/main.py
+++ app/main.py
@@ -1,26 +1,20 @@
 import math
 from typing import Any
 
+
 class OnlineCourse:
-   def __init__(
-           self,
-           name: str,
-           description: str,
-           weeks: int) -> None:
-       self.name = name
-       self.description = description
-       self.weeks = weeks
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
 
-   @staticmethod
-   def days_to_weeks(days: int) -> int:
-    return math.ceil(days / 7)
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        return math.ceil(days / 7)
 
-   @classmethod
-   def from_dict(cls, course_dict: dict) -> Any:
-       return cls(
-           name=course_dict.get("name"),
-           description=course_dict.get("description"),
-           weeks=OnlineCourse.days_to_weeks(
-               course_dict.get("days")
-           )
-       )
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> Any:
+        return cls(
+            name=course_dict.get("name"),
+            description=course_dict.get("description"),
+            weeks=OnlineCourse.days_to_weeks(course_dict.get("days")))
